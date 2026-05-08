f19f9853e06a1579602b7f06f38ebab2eb04b3a8
tomas m <mejiabolivar112@gmail.com>
2026-05-08 10:43:10 -0500

    Se agrega calculo de nomina y deducciones

--- nomina.py
+++ nomina.py
@@ -27,12 +27,38 @@ while True:
 
     elif opcion == "2":
 
-        print("Función en desarrollo...")
-
-    elif opcion == "3":
-
-        print("Saliendo del sistema...")
-        break
+        if len(empleados) == 0:
+            print("No hay empleados registrados")
 
     else:
-        print("Opción inválida")
+
+        for empleado in empleados:
+
+            salario_base = empleado["salario"]
+            dias = empleado["dias"]
+
+            salario_dias = (salario_base / 30) * dias
+
+            auxilio = 0
+
+            if salario_base < 2600000:
+                auxilio = 160000
+
+                descuento = salario_dias * 0.08
+
+                total_pagar = salario_dias + auxilio - descuento
+
+                print("\n===== DESPRENDIBLE =====")
+                print("Empleado:", empleado["nombre"])
+                print("Salario por días:", salario_dias)
+                print("Auxilio:", auxilio)
+                print("Descuento:", descuento)
+                print("Total a pagar:", total_pagar)
+
+            elif opcion == "3":
+
+                print("Saliendo del sistema...")
+                break
+
+            else:
+             print("Opción inválida")
